2222a91a7a2191050951fe67464d84aeac257f87
Matija Sabolić <matija.sabolic@ibm.com>
2025-07-23 09:59:15 +0200

    Supporting multiple tags in revalidate API

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -2,14 +2,19 @@ import { NextRequest, NextResponse } from "next/server";
 import { revalidateTag } from "next/cache";
 
 export async function POST(request: NextRequest) {
-  const requestHeaders = new Headers(request.headers);
-  const secret = requestHeaders.get("x-vercel-reval-key");
+    const requestHeaders = new Headers(request.headers);
+    const secret = requestHeaders.get("x-vercel-reval-key");
+    const tag = requestHeaders.get("x-vercel-reval-tag");
 
-  if (secret !== process.env.CONTENTFUL_PREVIEW_SECRET) {
-    return NextResponse.json({ message: "Invalid secret" }, { status: 401 });
-  }
+    if (!tag || !secret) {
+        return NextResponse.json({ message: "Bad request" }, { status: 400 });
+    }
 
-  revalidateTag("author");
+    if (secret !== process.env.CONTENTFUL_PREVIEW_SECRET) {
+        return NextResponse.json({ message: "Invalid secret" }, { status: 401 });
+    }
 
-  return NextResponse.json({ revalidated: true, now: Date.now() });
+    revalidateTag(tag);
+
+    return NextResponse.json({ revalidated: true, now: Date.now() });
 }
